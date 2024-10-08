b1fca5f63431a904a404dd8e5fa83cd55eec8951
Parker Lauders <parkerlauders04@gmail.com>
2024-10-08 10:53:12 -0400

    feat: Begin creating file system

--- disk.c
+++ disk.c
@@ -1,4 +1,5 @@
 #include "disk.h"
+#include "kprintf.h"
 
 #define PCI_DISK_CLASS 1
 #define PCI_DISK_SUBCLASS 1
@@ -194,4 +195,90 @@ void disk_read_sectors(
     } else {
         dispatch_request(req);
     }
+}
+
+// Filesystem items
+struct GUID efiBootGUID = {
+    0xc12a7328, 0xf81f, 0x11d2, 0xba4b,
+    { 0x00, 0xa0, 0xc9, 0x3e, 0xc9, 0x3b }
+};
+
+struct GUID windowsDataGUID = {
+    0xebd0a0a2, 0xb9e5, 0x4433, 0x87c0,
+    { 0x68, 0xb6, 0xb7, 0x26, 0x99, 0xc7 }
+};
+
+struct GUID linuxGUID = {
+    0x0fc63daf, 0x8483, 0x4772, 0x8e79,
+    { 0x3d, 0x69,0xd8, 0x47, 0x7d, 0xe4 }
+};
+
+static struct VBR vbr;
+static void read_vbr_callback( int errorcode, void* sectorData, void* kmain_callback) {
+    // Check for errors
+    if( errorcode != SUCCESS ){
+        kprintf("Cannot read VBR: %d\n",errorcode);
+        panic("Cannot continue");
+        return;
+    }
+
+    // Copy the VBR data out
+    kmemcpy(&vbr,sectorData,sizeof(vbr));
+
+    // Call the kmain call back
+    disk_metadata_callback_t f = (disk_metadata_callback_t) kmain_callback;
+    f();    
+}
+
+static void read_partition_table_callback(int errorcode, void* sectorData, void* kmain_callback) {
+    // Check for error
+    if( errorcode != SUCCESS ) {
+        kprintf("Cannot read partition table: %d\n",errorcode);
+        panic("Cannot continue");
+    }
+
+    // Set the sector data up as a array
+    struct GPTEntry* entry = (struct GPTEntry*) sectorData;
+
+    // Grab the first sector (VBR) out of the partition
+    disk_read_sectors( entry->firstSector, 1, read_vbr_callback, kmain_callback );
+}
+
+void disk_read_metadata( disk_metadata_callback_t kmain_callback ) {
+    disk_read_sectors( 2, 1, read_partition_table_callback, kmain_callback );
+}
+
+u32 clusterNumberToSectorNumber( u32 clnum ) {
+    // Ensure that the clnum is greater than two
+    if(clnum < 2)
+        panic("Bad cluster!");
+
+    // Get the starting location of the clusters
+    u32 clusterStart = vbr.first_sector + vbr.reserved_sectors + (vbr.sectors_per_fat * 2);
+
+    // Break down the cluster starting position
+    return clusterStart + (clnum - 2) * vbr.sectors_per_cluster;
+}
+
+static void callback(int errorCode, void* vdata, void* callback) {
+    if(errorCode) {
+        kprintf("ERROR: %d\n", errorCode);
+        return;
+    }
+
+    char* data = (char*)vdata;
+
+    for(int i = 0; i < 512; i++) {
+        kprintf("%c", data[i]);
+    }
+
+    kprintf("\nDONE\n");
+}
+
+void readRoot() {
+    // Get the root sector num
+    u32 rootSectorNum = clusterNumberToSectorNumber(vbr.root_cluster);
+
+    // Read the root sector
+    disk_read_sectors(rootSectorNum, 1, callback, 0);
 }
